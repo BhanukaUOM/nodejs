c4fa3222bb84a24e335a27d3cf2378a0272446d3
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-22 20:33:33 +0530

    Express Route Params

--- src/8. Express.js
+++ src/8. Express.js
@@ -9,4 +9,8 @@ app.get('/admin', function(req, res){
     res.send("Administrator Page");
 });
 
+app.get('/id/:id', function(req, res){
+    res.send(`ID is: ${req.params.id}`);
+});
+
 app.listen(3000);
