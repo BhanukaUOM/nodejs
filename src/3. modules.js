56b11af7cd4dcf290eae667cfeff64c1c7b52259
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-22 12:53:14 +0530

    1 module

--- src/3. modules.js
+++ src/3. modules.js
@@ -1,4 +1,7 @@
+/* for 1 module
+
 var counter = require('./module');
 
 console.log(counter(['ah', 'bh', 'nm']));
 
+*/
--- src/module.js
+++ src/module.js
@@ -1,5 +1,9 @@
+/* for 1 module
+
 var counter = function(arr){
     return "There are " + arr.length + " elements in this array";
 }
 
 module.exports = counter;
+
+*/
