82700058aea80d6e39d3695f87abf0df90bf72c1
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-22 18:25:15 +0530

    Send JSON to Server

--- src/7. pipe to server.js
+++ src/7. pipe to server.js
@@ -1,10 +1,24 @@
 var fs = require('fs');
 var http = require('http');
 
+/* Text, HTML
+
 var server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type' : 'text/plain'});  //text/html for HTML Files
     var streamRead = fs.createReadStream(__dirname + "module.js", 'utf8'); //module.html fot HTML Files
     streamRead.pipe(res);
+});
+*/
+
+
+/* JSON */
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type' : 'application/json'});
+    var obj = {
+        name: "BMCSoft",
+        address: "LK"
+    };
+    res.end(JSON.stringify(obj));
 });
 
 server.listen(3000, '127.0.0.1');
