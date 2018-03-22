665e1b488ae486a5c9d640f16648d0cd02357d8a
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-22 21:11:56 +0530

    EJS Template Engine in Express

---
+++ src/9. ejs.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.send("Home Page");
+});
+
+app.get('/admin', function(req, res){
+    res.sendFile(__dirname + "/module.js");
+});
+
+app.get('/id/:id', function(req, res){
+    var object = {'name' : 'name', 'age' : 23};
+    res.render('person', {id : req.params.id, obj : object});
+});
+
+app.listen(3000);
