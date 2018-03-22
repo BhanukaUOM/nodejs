731c2f8aff0b0f247e9e60ecbbbaf338a70284cc
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-22 17:44:51 +0530

    HTML to Server

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 2. Function Expressions : Normal Function, Function Expressions, Call Function in Function
 3. Modules : 1 Module, More Modules, More Modules without Variable Passing, Module export using Object Notation
 4. Core Modules: R/W, Delete File(fs, sync, async), Create, Remove Directories(Sync, Async)
+5. Create a Server
+6. Stream Files: R/W, Pipe
+7. Use Pipe to set server: Text and HTML
+8. 
--- src/7. pipe to server.js
+++ src/7. pipe to server.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type' : 'text/plain'});
-    var streamRead = fs.createReadStream(__dirname + "module.js", 'utf8');
+    res.writeHead(200, {'Content-Type' : 'text/plain'});  //text/html for HTML Files
+    var streamRead = fs.createReadStream(__dirname + "module.js", 'utf8'); //module.html fot HTML Files
     streamRead.pipe(res);
 });
 
