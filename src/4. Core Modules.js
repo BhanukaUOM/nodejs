c07963da6ce80992cf5b9571c61a07850423e33d
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-22 14:38:00 +0530

    Create Remove Directories

--- src/4. Core Modules.js
+++ src/4. Core Modules.js
@@ -1,7 +1,7 @@
 /* fs module  */
 var fs = require('fs');
 
-
+// Read Write File
 //Sync: blocking Code
 var modules = fs.readFileSync("module.js", "utf8");
 console.log(modules);
@@ -11,6 +11,23 @@ fs.writeFileSync("a.txt", "Write as Sync");
 //ASync: Multiple requests
 fs.readFile("module.js", 'utf8', function(err, data){
     console.log(data);
+    fs.writeFile("a.txt", "Write as Async", function(){});
 });
 
-fs.writeFile("a.txt", "Write as Async");
+//Delete File
+fs.unlink('a.txt');  //if file not exists get error
+
+
+// Make Remove Directory Sync: block code
+fs.mkdirSync("abc");  
+fs.rmdirSync("abc");
+
+// Make Remove Directory Async
+fs.mkdir("abc", function(){
+    fs.readFile("module.js", 'utf8', function(err, data){
+        fs.writeFile('./abc/a.txt', data, function(){
+            fs.unlink('./abc/a.txt');
+            fs.rmdir("abc");  //can't remove non Empty Directives    
+        });
+    });
+});  
