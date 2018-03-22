2a685dce6e19f7a5cfde48d728589b855aa45b8d
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-22 17:16:22 +0530

    Stream as Pipe

--- src/6. streams.js
+++ src/6. streams.js
@@ -3,8 +3,11 @@ var fs = require('fs');
 var readStream = fs.createReadStream(__dirname + "/module.js", 'utf-8');
 var writeStream = fs.createWriteStream(__dirname + "/a.txt");
 
-
+/*
 readStream.on('data', function(chunk){
     console.log("Chunk Received");
     writeStream.write(chunk);
 });
+*/
+
+readStream.pipe(writeStream);
