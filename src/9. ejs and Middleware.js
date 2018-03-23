0311a757292ccf7f9a602209c068a21c365d5f53
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-23 13:12:17 +0530

    middleware

--- src/9. ejs and Middleware.js
+++ src/9. ejs and Middleware.js
@@ -3,6 +3,11 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+app.use('/id', function(req, res, next){
+    console.log(req.url);
+    next(); // run next codes eg: var object = {'name' : 'name', 'age' : [23,24,25,26,10]}; res.render('person', {id : req.params.id, obj : object});
+});
+
 app.get('/', function(req, res){
     res.send("Home Page");
 });
