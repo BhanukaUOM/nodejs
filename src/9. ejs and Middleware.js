5c87d84ebd39cb9f91eac977d531423dc6ca07f6
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-03-23 13:16:49 +0530

    Express Static Files

--- src/9. ejs and Middleware.js
+++ src/9. ejs and Middleware.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
-
+/* Middleware 
 app.use('/id', function(req, res, next){
     console.log(req.url);
     next(); // run next codes eg: var object = {'name' : 'name', 'age' : [23,24,25,26,10]}; res.render('person', {id : req.params.id, obj : object});
 });
+*/
+
+/* static files */
+app.use('/views', express.static('views'));
 
 app.get('/', function(req, res){
     res.send("Home Page");
